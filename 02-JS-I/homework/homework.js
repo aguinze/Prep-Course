b5f7f47f6334ebec1e64cd26d8f90b5debbe5d6b
aguinze <109623864+aguinze@users.noreply.github.com>
2022-11-19 10:13:38 -0300

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -152,10 +152,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(numero%2===0){
-    return "Es positivo"
-  }else{
-    return "Es negativo"
+  if(numero>0){
+    return "Es positivo";
+  }
+  if(numero<0){
+    return "Es negativo";
+  }
+  else{
+    return false;
   }
 }
 
@@ -183,18 +187,21 @@ function obtenerSaludo(nombre) {
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  return alto * ancho;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
-  //Escribe tu código aquí
+  //Escribe tu código aquíç
+  return lado + lado + lado + lado;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
+  return (base*altura)/2;
 }
 
 
@@ -202,6 +209,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
+  return euro*1.2;
 }
 
 
@@ -210,7 +218,16 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
-  //Escribe tu código aquíffffffffffffffffffffffffff
+  //Escribe tu código aquí:
+  if(letra==="a" || letra==="e" || letra==="i" || letra==="o" || letra==="u"){
+    return "Es vocal";
+  }
+  if(letra.length>1){
+    return "Dato incorrecto";
+  }
+  else{
+    return "Dato incorrecto";
+  }
 }
 
 
